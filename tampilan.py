e40e5374cfbac25493d9b0d0417406c3edcc3dfd
M. Ramadhan Kurniawan <ramadhank421@gmail.com>
2026-02-23 17:17:28 +0700

    update modul tampilan.py dan sedikit main.py oleh rama (111)

---
+++ tampilan.py
@@ -0,0 +1,27 @@
+import time
+
+def opening():
+    print("=" * 65)
+    print("P R O Y E K : T E M B O K  R A T A P A N  S O L O")
+    print("=" * 65)
+    print("Tahun 202x. Sebuah rezim baru bertahta di balik tembok tebal.")
+    print("Istaga dijaga ketat oleh Pasukan Termul hingga Sang Pangeran Soloman")
+    print("Senjata yang mereka gunakan bukanlah pedang, melainkan Matematika Ekstrem!")
+    print("Siapkan mentalmu untuk blusukan menembus Ring 1 Istana!")
+    print("-" * 65)
+    time.sleep(1.5)
+    print("\n[SISTEM DUKCAPIL] Memindai data pemberontak... eh, pahlawan...\n")
+
+def game_over(nama):
+    print("\n" + "📉"*15)
+    print("B U Z Z E R  A L E R T : M I S I  G A G A L !")
+    print(f"Elektabilitas Saudara {nama} hancur lebur di hadapan penguasa.")
+    print("Anda ditangkan dan berakhir menjadi pekerja lepas di perkebunan sawit kerajaan.")
+    print("📉"*15)
+
+def tamat(nama):
+    print("\n" + "👑"*15)
+    print("R E V O L U S I  B E R H A S I L !")
+    print(f"Selamat! Anda berhasil meruntuhkan kejayaan Temple of Soloman!")
+    print("Anda kini memegang kunci Temple of Soloman dan membebaskan kebun sawit kerajaan")
+    print("👑"*15)
