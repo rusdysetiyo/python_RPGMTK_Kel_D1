764a28262a67c3c7631bdf8a41b7aba36fe01a50
M. Ramadhan Kurniawan <ramadhank421@gmail.com>
2026-02-24 06:57:47 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,4 +2,47 @@ import player
 import stage_data
 import battle
 import tampilan
+import evaluasi
 import time
+
+def jalankan_game():
+    tampilan.disclaimer()
+    
+    tampilan.opening()
+    
+    nama_pahlawan = input("Masukkan NIK / Identitas Rahasia Anda: ").strip()
+    hero = player.buat_karakter(nama_pahlawan)
+    
+    print(f"\n[SISTEM] Verifikasi berhasil. Otorisasi sementara diberikan kepada [{hero['nama']}].")
+    print("[SISTEM] Membuka gerbang utama menuju Ring 1...")
+    time.sleep(2)
+    
+    level_sekarang = 1
+    
+    while True:
+        stage_saat_ini = stage_data.get_stage(level_sekarang)
+        
+        if stage_saat_ini == None:
+            tampilan.tamat(hero['nama'])
+            evaluasi.cetak_raport(hero['nama'], hero['nyawa'], level_sekarang - 1, True)
+            break
+
+        print(f"\n" + "="*50)
+        print(f"--- 🛑 MEMASUKI KAWASAN RING {level_sekarang} 🛑 ---")
+        time.sleep(1)
+        
+        
+        hasil_menang = battle.hadapi_monster(hero, stage_saat_ini)
+
+        if hasil_menang == True:
+            print("\n[SISTEM] Otorisasi Ring disetujui! Lanjut blusukan lebih dalam...")
+            level_sekarang += 1
+            time.sleep(1.5)
+            
+        else:
+            tampilan.game_over(hero['nama'])
+            evaluasi.cetak_raport(hero['nama'], hero['nyawa'], level_sekarang, False)
+            break
+
+if __name__ == "__main__":
+    jalankan_game()
